4ab287ec51de292a61e3b053c976f5963165c362
EshtaKhadgi <eshtashai11@gmail.com>
2021-03-01 08:13:36 +0545

    check

--- app/src/test/java/com/kiran/testingkotlinapp/MyTest.kt
+++ app/src/test/java/com/kiran/testingkotlinapp/MyTest.kt
@@ -1,4 +1,27 @@
 package com.kiran.testingkotlinapp
 
+import org.junit.Assert
+import org.junit.Test
+
 class MyTest {
+    private lateinit var arithmetic: Arithmetic
+    // addition test
+    @Test
+    fun testAddition(){
+        val maileExpectGareko = 7
+        arithmetic = Arithmetic()
+        arithmetic.first = 3
+        arithmetic.second = 2
+        val actualResult = arithmetic.add()
+        Assert.assertEquals(maileExpectGareko,actualResult)
+    }
+    @Test
+    fun testSubtract(){
+        val maileExpectGareko =1
+        arithmetic = Arithmetic()
+        arithmetic.first = 3
+        arithmetic.second = 2
+        val actualResult = arithmetic.subtract()
+        Assert.assertEquals(maileExpectGareko,actualResult)
+    }
 }
